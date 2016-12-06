6098614ca3553c70dd6b718fc28900cd8545bae9
Yaoli Wang <yaoliwang2@gmail.com>
2016-12-06 10:50:38 -0800

    fixed according to comments

--- housing_django/log/forms.py
+++ housing_django/log/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 # If you don't do this you cannot use Bootstrap CSS
 class LoginForm(AuthenticationForm):
-    username = forms.CharField(label="Username", max_length=30, 
+    username = forms.CharField(label='Username', max_length=30, 
                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
-    password = forms.CharField(label="Password", max_length=30, 
+    password = forms.CharField(label='Password', max_length=30, 
                                widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
--- housing_django/log/views.py
+++ housing_django/log/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-@login_required(login_url="login/")
+@login_required(login_url='login/')
 def home(request):
-  return render(request,"home.html")
-  
+  return render(request,'home.html')
