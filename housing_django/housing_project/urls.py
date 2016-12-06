6dd05022ace05cc9c0af21f832c65761c365a1b3
Yaoli Wang <yaoliwang2@gmail.com>
2016-12-05 21:39:51 -0800

    User system set up

--- housing_django/housing_project/urls.py
+++ housing_django/housing_project/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include, url
+from django.contrib.auth import views
+from log.forms import LoginForm
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('log.urls', namespace="log")),
+    url(r'^login/$', views.login, {'template_name': 'login.html', 'authentication_form': LoginForm }, name='login'),  
+    url(r'^logout/$', views.logout, {'next_page': '/login'}),  
 ]
